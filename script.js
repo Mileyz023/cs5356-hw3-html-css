314ca7597c713435d71e19962ab334d1b5950c43
Yu Zhang <miley.zhang@mail.utoronto.ca>
2025-02-26 16:03:10 -0500

    update api fetching

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const detailsElement = document.getElementById("quote-section");
-    const quoteIntro = document.getElementById("quote-intro");
     const quoteText = document.getElementById("kanye-quote");
 
     detailsElement.addEventListener("toggle", function () {
         if (detailsElement.open) {
-            quoteIntro.style.display = "block";
 
             fetch("https://api.kanye.rest/")
                 .then(response => response.json())
